c971df98dce7dc94cd107fe0ba1085efc5bcad05
Akshaya Balineni <balineni_a@India.XoriantCorp.com>
2022-09-26 10:02:38 +0530

    jwt

--- student-enrollment-application/src/main/java/com/xoriant/enrollmentapplication/springSecurityJWT/MyUserDetailsService.java
+++ student-enrollment-application/src/main/java/com/xoriant/enrollmentapplication/springSecurityJWT/MyUserDetailsService.java
@@ -10,10 +10,9 @@ import java.util.ArrayList;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
-
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new User("foo", "foo",
+        return new User("java", "java",
                 new ArrayList<>());
     }
 }
