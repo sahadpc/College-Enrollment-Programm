a67fa5211e19f716aba62c775bff25549544fe53
Sahad cheruvampatta <cheruvampatta_s@India.XoriantCorp.com>
2022-09-29 11:05:07 +0530

    Finalcommit

---
+++ src/src/app/app.module.ts
@@ -0,0 +1,77 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule , HTTP_INTERCEPTORS} from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import {ConfirmEqualValidatorDirective} from './shared/conform-equal-validatot.directive';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import {AuthInterceptor} from './auth/auth.interceptor';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ConfirmEqualValidatorDirective,
+    PagenotfoundComponent,
+    DashboardComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+  
+    
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    LayoutModule
+
+
+
+
+
+
+    
+  ],
+  providers: [AuthGuard,
+  {
+    provide:HTTP_INTERCEPTORS,
+    useClass:AuthInterceptor,
+    multi:true
+
+ }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
